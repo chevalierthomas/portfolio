619e9bb538d426654db36cf09c260fe3f4b92f8c
Thomas Chevalier <93100327+chevalierthomas@users.noreply.github.com>
2025-10-17 02:08:27 +0200

    fix: configure aliases and dark styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
@@ -7,6 +8,16 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro'
+    }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+        '@styles': fileURLToPath(new URL('./src/styles', import.meta.url))
+      }
     }
   }
 });
